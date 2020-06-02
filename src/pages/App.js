e1fb71de3d8c365d82b5dd91daaba1e388f1e320
JuanTorres8718 <jdtorres8718@gmail.com>
2020-06-02 13:04:09 -0500

    create folder component and Content

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './styles/App.css';
+import '../styles/App.css';
+import Content from '../components/Content';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Content />
   );
 }
 
