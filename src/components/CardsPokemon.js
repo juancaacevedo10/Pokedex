c9dc0be686e2dc61c07c54e6f29256005774e3a8
JuanTorres8718 <jdtorres8718@gmail.com>
2020-06-02 17:53:04 -0500

    add fetch

--- src/components/CardsPokemon.js
+++ src/components/CardsPokemon.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const CardsPokemon = () => {
     return ( 
+        <div>
         <h3>What is that pokemon?</h3>
+        </div>
+        
      );
 }
  
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import CardsPokemon from './CardsPokemon';
 
 const Content = () => {
-    return ( 
+
+    let [pokemons,setPokemons]= useState([
+
+    ])
+
+    useEffect(()=>{
+      let array = []
+      const fetchData= async()=>{
+        for (let i = 1; i < 26; i++) {
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            let data = await response.json()
+            array.push(data)
+        }
+
+        setPokemons(array)
+      }
+      fetchData()  
+    },[])
+
+    
+    return (
         <div>
             <h1>Pokedex</h1>
-            <CardsPokemon />
+            {pokemons.map((pokemon, index)=>{
+              return  <CardsPokemon 
+                key = {index}
+                name = {pokemon.name}
+                picture = {pokemon.sprites.front_default}
+                number = {pokemon.id}
+                types = {pokemon.types}
+                />
+            })}
+           
         </div>
-     );
+      );
 }
  
 export default Content;
