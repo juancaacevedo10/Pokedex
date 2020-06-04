476d72d6b24d41838fb1bd498b0899d6f8f5cc82
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-06-04 00:59:52 -0500

    Updates

---
+++ precache-manifest.ea28b24375927d0a48f7e14f720a17d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15173208dd1d2250712d98a63054895",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "3ce9127d97af3edaa525",
+    "url": "/Pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9e545b79886e13c25967",
+    "url": "/Pokedex/static/css/main.12ec692e.chunk.css"
+  },
+  {
+    "revision": "3ce9127d97af3edaa525",
+    "url": "/Pokedex/static/js/2.a3022c62.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Pokedex/static/js/2.a3022c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e545b79886e13c25967",
+    "url": "/Pokedex/static/js/main.a142dc90.chunk.js"
+  },
+  {
+    "revision": "1c8a54ccf81f15e85b43",
+    "url": "/Pokedex/static/js/runtime-main.75842f26.js"
+  }
+]);
